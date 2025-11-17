d0f6463b12770bc3cac636d1faeda6a12989d5f4
uday vamsi <udayvamsi07@gmail.com>
2025-11-17 16:06:43 -0600

    hibernate

--- src/main/java/com/hb/App.java
+++ src/main/java/com/hb/App.java
@@ -12,21 +12,34 @@ public class App
     {
     
         Student obj=new Student();
-        obj.setSid(5);
-        obj.setName("jack");
-        obj.setMarks(55);
-        Configuration cfg=new Configuration();
-        cfg.addAnnotatedClass(com.hb.model.Student.class);
-        cfg.configure();
-      SessionFactory sf=cfg.buildSessionFactory();
+       // Student obj1=null;
+        obj.setSid(7);
+        obj.setName("eve");
+        obj.setMarks(100);
+    //    Configuration cfg=new Configuration();
+    //     cfg.addAnnotatedClass(com.hb.model.Student.class);
+    //    cfg.configure();
+    //   SessionFactory sf=cfg.buildSessionFactory();
+// or you can write like this
+SessionFactory sf=new Configuration()
+.addAnnotatedClass(com.hb.model.Student.class)
+.configure()
+.buildSessionFactory();
+      
         Session s=sf.openSession();
         // Transaction t=s.beginTransaction();
-        Transaction t=s.beginTransaction();
+      //  Transaction t=s.beginTransaction();
+      //s.persist(obj);
+       // t.commit();
+     //  obj1=s.get(Student.class, 7);
+Transaction t=s.beginTransaction();
+s.merge(obj);//to update any data into database
+t.commit();
 
-        s.persist(obj);
-        t.commit();
-
-        System.out.println(obj);
+        
+        s.close();
+        sf.close();
+       // System.out.println(obj1.getName());
 
         
     }
